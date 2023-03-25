4742757555773bc68cb971ccc77e6595b9ce0bf7
balla prasanna kumar <99463539+bprasanna881@users.noreply.github.com>
2023-03-25 17:19:46 +0530

    Create input.tf

---
+++ input.tf
@@ -0,0 +1,32 @@
+variable "subscription_id" {
+    type = string
+    default = "7bb8b645-9009-4b9e-b9bd-c047d8140598"
+    description = "PROD subcription id"
+  
+}
+
+variable "client_id" {
+    type = string
+    default = "fb7ec1db-e02d-499a-bfac-14aff67b5c06"
+    description = "client id"
+  
+}
+
+variable "client_secret" {
+    type = string
+    default = "Cqy8Q~mqCtUiw.rCpLdp4qbrP6LmJWnyGaPsWbev  "
+    description = "client secret"
+  
+}
+
+variable "tenant_id" {
+    type = string
+    default = "917ef614-9aed-4923-a6cf-285178702be9"
+    description = "tenant id"
+  
+}
+variable "location" {
+  type        = string
+  default = "East US"
+  description = "Rg location"
+}
